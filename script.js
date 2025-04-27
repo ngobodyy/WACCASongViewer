4d4d2866eb5c9b188571aabf30561048559f6ddd
ngo <ngobodyy@gmail.com>
2025-04-27 04:43:09 -0400

    test

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ for (let s in waccaSongs){
 
 dropdown.addEventListener("change", (event)=>{
     let songKey = event.target.value
-    document.getElementById("displayInfo").innerHTML = `
+    document.getElementById("displayInfo1").innerHTML = `
         <h3>${waccaSongs[songKey].title}</h3>
+        `
+     if (waccaSongs[songKey].titleEnglish != null) {
+        document.getElementById("displayEngTitle").innerHTML = `
+        <h3>${waccaSongs[songKey].titleEnglish}</h3>
+        `
+    }
+    document.getElementById("displayInfo2").innerHTML = `
         <h3>${waccaSongs[songKey].artist}</h3>
         <img class=albumart src="/WACCA_Songs/img/${waccaSongs[songKey].imageName}">
+        <div></div>
         <audio controls src="/WACCA_Songs/audio/${waccaSongs[songKey].audioPath}"></audio>
-        <audio controls src="/WACCA_Songs/audio/${waccaSongs[songKey].audioInfPath}"></audio>   
+    `
+    if (waccaSongs[songKey].audioInfPath != null) {
+        document.getElementById("displayInfAudio").innerHTML = `
+        <audio controls src="/WACCA_Songs/audio/${waccaSongs[songKey].audioInfPath}"></audio>
+        `
+    }
+    document.getElementById("displayInfo3").innerHTML = `
+        <h3>Song ID: ${waccaSongs[songKey].id}</h3>
+        <h3>Category: ${waccaSongs[songKey].category}</h3>
+        <h3>Release Date: ${waccaSongs[songKey].releaseDate}</h3>
+        <h3>Song BPM: ${waccaSongs[songKey].bpm}</h3>
     `
 })
